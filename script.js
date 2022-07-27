747be18ccd1a2fe64edbb68506eed182bfd3bbfe
VlastikYoutubeKo <56507931+VlastikYoutubeKo@users.noreply.github.com>
2022-07-27 23:29:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/5d6eadbcca47afbcb42ab1b75/90b0b7d3e20d046351f7b563a.js");
